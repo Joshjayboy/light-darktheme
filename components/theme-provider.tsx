48bbafecc2464a3c5c7f77c9eecc96f3c5f333fd
joshjayboy <jakintemi@gmail.com>
2025-01-27 20:55:44 +0100

    comment on theme-provider page

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,9 +1,19 @@
+// Use "use client" to enable client-side rendering for this file
 "use client";
 
+// Import React for component creation
 import * as React from "react";
+
+// Import the ThemeProvider from the 'next-themes' package for theme management
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+// Import the type definition for ThemeProviderProps from the 'next-themes' package
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    // Pass all received props to the NextThemesProvider and render the children within it
+    <NextThemesProvider {...props}>{children}</NextThemesProvider>
+
+  )
 }
