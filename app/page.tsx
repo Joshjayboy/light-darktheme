5ec39e39ec57cdd17292b2003a8b9394c0a7de0f
joshjayboy <jakintemi@gmail.com>
2025-01-27 21:52:52 +0100

    comment on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
-
+// Import the MainContent component from the components directory
 import MainContent from "@/components/MainContent";
 
+/**
+ * Home Component
+ *
+ * This is the default export for the `Home` page. 
+ * It serves as the main entry point for rendering the primary content of the application.
+ *
+ * @returns {JSX.Element} The main content of the application, rendered using the MainContent component.
+ */
 export default function Home() {
+  // Render the MainContent component as the primary content for the Home page
   return <MainContent />;
 }
